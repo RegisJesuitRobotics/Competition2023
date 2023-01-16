432a9321d1228dbc763219e38570b74fa6e8f64c
ohowe <ohowe@duck.com>
2023-01-16 14:35:31 -0700

    Formatting

--- src/main/java/frc/robot/subsystems/lift/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/lift/LiftSubsystem.java
@@ -1,24 +1,20 @@
 package frc.robot.subsystems.lift;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LiftConstants;
 
 public class LiftSubsystem extends SubsystemBase {
-    private final CANSparkMax topLeftMotor =
-            new CANSparkMax(LiftConstants.TOP_LEFT_MOTOR_PORT, MotorType.kBrushless);
+    private final CANSparkMax topLeftMotor = new CANSparkMax(LiftConstants.TOP_LEFT_MOTOR_PORT, MotorType.kBrushless);
 
     private final CANSparkMax bottomLeftMotor =
             new CANSparkMax(LiftConstants.BOTTOM_LEFT_MOTOR_PORT, MotorType.kBrushless);
 
-    private final CANSparkMax topRightMotor =
-            new CANSparkMax(LiftConstants.TOP_RIGHT_MOTOR_PORT, MotorType.kBrushless);
+    private final CANSparkMax topRightMotor = new CANSparkMax(LiftConstants.TOP_RIGHT_MOTOR_PORT, MotorType.kBrushless);
 
     private final CANSparkMax bottomRightMotor =
             new CANSparkMax(LiftConstants.BOTOM_RIGHT_MOTOR_PORT, MotorType.kBrushless);
 
-    public LiftSubsystem() {
-    }
+    public LiftSubsystem() {}
 }
