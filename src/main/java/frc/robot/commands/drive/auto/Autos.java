81b7bf9ec22d61f64aa6ed868c37bf3fb02820d4
ohowe <ohowe@duck.com>
2023-02-04 15:32:31 -0700

    Merge branch 'main' into workingPathGen

--- src/main/java/frc/robot/commands/drive/auto/Autos.java
+++ src/main/java/frc/robot/commands/drive/auto/Autos.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.drive.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.MiscConstants;
 import frc.robot.commands.drive.GreaseGearsCommand;
 import frc.robot.commands.drive.characterize.*;
 import frc.robot.subsystems.swerve.SwerveDriveSubsystem;
@@ -11,10 +12,12 @@ public class Autos {
     private final HashMap<String, Command> autoMap = new HashMap<>();
 
     public Autos(SwerveDriveSubsystem driveSubsystem) {
-        addAuto("SysIDCharacterization", new DriveTrainSysIDCompatibleLoggerCommand(driveSubsystem));
-        addAuto("SteerTesting", new SteerTestingCommand(driveSubsystem));
-        addAuto("DriveTestingCommand", new DriveTestingCommand(1.0, true, driveSubsystem));
-        addAuto("GreaseGears", new GreaseGearsCommand(driveSubsystem));
+        if (MiscConstants.TUNING_MODE) {
+            addAuto("SysIDLogger", new DriveTrainSysIDCompatibleLoggerCommand(driveSubsystem));
+            addAuto("GreaseGears", new GreaseGearsCommand(driveSubsystem));
+            addAuto("DriveTestingCommand", new DriveTestingCommand(1.0, true, driveSubsystem));
+            addAuto("SteerTesting", new SteerTestingCommand(driveSubsystem));
+        }
     }
 
     private void addAuto(String name, Command command) {
